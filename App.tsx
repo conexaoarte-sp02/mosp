d68717457edd907ef0bd89079da2029689845d37
Anthony Rafael <anthonyribeiro1910@gmail.com>
2021-11-24 00:05:29 -0300

    First Class

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View>
+            <Text style={styles.title}>Descubra</Text>
+            <Text style={styles.title}>lugares incríveis</Text>            
+          </View>
+          <View>
+            <Image
+              style={styles.profile}
+              source={require('./assets/images/profile.png')}
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%',
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingBottom: 50,
+    paddingTop: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    height: 45,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 10,
+  },
+  title: {
+    color: '#000',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  profile: {
+    width: 38,
+    height: 38,
   },
 });
